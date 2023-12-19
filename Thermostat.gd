6b75ac4b6f41c56ac00547a3b49c655965e34dbf
ARGO\Hayden <thrownyonder@gmail.com>
2023-12-18 20:39:30 -0600

    Added logic to move temp up and down, set the temp, set the mode. I also wired all the buttons, and set a display on screen for the current indoor temp.

--- Thermostat.gd
+++ Thermostat.gd
@@ -1,11 +1,43 @@
 extends Sprite2D
 
+var isCooling = true
+var currentTemp = 71
+var setTemp = 70
+var tempChangeRate = 0.1
+var indoorTempLabel
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+func _process(delta):
+	indoorTempLabel = get_node("IndoorTempLabel")
+	_ac_operation(delta)
+	
+	_update_indoor_temp_label()
+	
+	print(currentTemp)
+
+func _on_temp_up_button_pressed():
+	setTemp += 1
+
+func _on_mode_button_pressed():
+	if isCooling:
+		isCooling = false
+	else:
+		isCooling = true
+		
+	print(isCooling)
+
+func _on_temp_down_button_pressed():
+	setTemp -= 1
+
+func _ac_operation(delta):
+		if isCooling and currentTemp > setTemp:
+			currentTemp -= tempChangeRate * delta
+		
+		if not isCooling and currentTemp < setTemp:
+			currentTemp += tempChangeRate * delta
+
+func _update_indoor_temp_label():
+	var intTemp = int(currentTemp)
+	indoorTempLabel.text = "Indoor Temp: " + str(intTemp)
